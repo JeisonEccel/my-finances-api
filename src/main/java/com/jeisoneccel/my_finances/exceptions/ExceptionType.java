63bb1268118363c080f8b22b8c5ab99f1828758e
Jeison Eccel <projetojeison@hotmail.com>
2025-09-17 09:11:01 -0700

    Refs #4: Added exception handler for RecordAlreadyExists

--- src/main/java/com/jeisoneccel/my_finances/exceptions/ExceptionType.java
+++ src/main/java/com/jeisoneccel/my_finances/exceptions/ExceptionType.java
@@ -2,5 +2,6 @@ package com.jeisoneccel.my_finances.exceptions;
 
 public enum ExceptionType {
     BAD_CREDENTIALS,
+    RECORD_ALREADY_EXISTS,
     RECORD_NOT_FOUND
 }
--- src/main/java/com/jeisoneccel/my_finances/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/jeisoneccel/my_finances/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.jeisoneccel.my_finances.exceptions;
 
+import com.jeisoneccel.my_finances.exceptions.custom.RecordAlreadyExistsException;
 import com.jeisoneccel.my_finances.exceptions.custom.RecordNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static com.jeisoneccel.my_finances.exceptions.ExceptionType.BAD_CREDENTIALS;
-import static com.jeisoneccel.my_finances.exceptions.ExceptionType.RECORD_NOT_FOUND;
+import static com.jeisoneccel.my_finances.exceptions.ExceptionType.*;
 import static org.springframework.http.HttpStatus.*;
 
 @Slf4j
@@ -50,6 +50,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         String error = ex.getLocalizedMessage();
 
         RequestError requestError = new RequestError(NOT_FOUND, RECORD_NOT_FOUND, error);
+        return new ResponseEntity<>(requestError, new HttpHeaders(), requestError.getHttpStatus());
+    }
+
+    @ExceptionHandler({RecordAlreadyExistsException.class})
+    public ResponseEntity<Object> handleRecordAlreadyExists(Exception ex, WebRequest request) {
+        String error = ex.getLocalizedMessage();
+
+        RequestError requestError = new RequestError(UNPROCESSABLE_ENTITY, RECORD_ALREADY_EXISTS, error);
         return new ResponseEntity<>(requestError, new HttpHeaders(), requestError.getHttpStatus());
     }
 
