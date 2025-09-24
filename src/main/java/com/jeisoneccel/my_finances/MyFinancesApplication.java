d141c560d2e5b727cadb0810674dec4d2fdae2f0
Jeison Eccel <projetojeison@hotmail.com>
2025-09-24 11:13:10 -0700

    Refs #11: Created static appContext

--- src/main/java/com/jeisoneccel/my_finances/MyFinancesApplication.java
+++ src/main/java/com/jeisoneccel/my_finances/MyFinancesApplication.java
@@ -2,12 +2,15 @@ package com.jeisoneccel.my_finances;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MyFinancesApplication {
 
+	public static ConfigurableApplicationContext appContext;
+
 	public static void main(String[] args) {
-		SpringApplication.run(MyFinancesApplication.class, args);
+		appContext = SpringApplication.run(MyFinancesApplication.class, args);
 	}
 
 }
